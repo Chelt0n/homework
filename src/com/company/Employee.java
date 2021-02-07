74236cec0152b2cc548fe6e02c55c473dd20d980
Chelt0n <qys1@yandex.ru>
2021-02-07 13:52:48 +0300

    end

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -7,12 +7,77 @@ public class Employee {
     private String position;
     private String email;
     private long number;
-    private int salary;
+    private double salary;
     private int age;
 
-    Employee(String name, String position, String email, long number, int salary, int age){
-        System.out.println();
+    public Employee(String name, String position, String email, long number, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.number = number;
+        this.salary = salary;
+        this.age = age;
+
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", number=" + number +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Employee employee;
+        Employee employee = new Employee("Иван", "java разараб", "q@ya.ru", 999622505, 30000, 45);
 
+        Employee[] empArray = new Employee[5];
+        empArray[0] = new Employee("Иван", "java разараб", "q@ya.ru", 999622505, 30000, 45);
+        empArray[1] = new Employee("Сергей", "java разараб", "q@ya.ru", 999622505, 30000, 30);
+        empArray[2] = new Employee("Дмитрий", "java разараб", "q@ya.ru", 999622505, 30000, 44);
+        empArray[3] = new Employee("Олег", "java разараб", "q@ya.ru", 999622505, 30000, 25);
+        empArray[4] = new Employee("Жмых", "java разараб", "q@ya.ru", 999622505, 30000, 15);
+        for (Employee employers : empArray) {
+            if(employers.getAge()> 40){
+                System.out.println(employers.toString());
+            }
+        }
     }
 }
