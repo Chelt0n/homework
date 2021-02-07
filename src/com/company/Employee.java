c908ae42df05d4be8947b215adcbf88ebadb9384
Chelt0n <qys1@yandex.ru>
2021-02-07 11:56:20 +0300

    create main

---
+++ src/com/company/Employee.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.Console;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private long number;
+    private int salary;
+    private int age;
+
+    Employee(String name, String position, String email, long number, int salary, int age){
+        System.out.println();
+    }
+
+}
+
